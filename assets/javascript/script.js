d579a76ae9adb61e6ad4a71ac542df405adec697
Gabriel Pedro <gpedro831@gmail.com>
2015-08-13 22:05:12 -0400

    Preparing to isolated header and footer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,46 @@
 /**
- * Put you theme js here or concat multiples js files to this file
+ * (CSP) Cdp Static Parts
+ *
+ * Integração de visual, barra e footer dos projetos da CdP
  */
-console.info('theme-js');
+
+(function () {
+
+var cdp = window.cdp || {};
+
+cdp.readCookie = function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
+
+
+cdp.renderUserMenuLinks = function renderUserMenuLinks() {
+  var token = cdp.readCookie('wetoken') || '';
+  $.ajax({
+    url: '/render-menu',
+    method: 'GET',
+    xhrFields: {
+        withCredentials: true
+    },
+    success: function(data) {
+      $('#main-navbar-content').append(data.header);
+      $('#main-footer-content').append(data.footer);
+    },
+    fail: function (err) {
+      console.error('Failed to load navbar and menu', err);
+    }
+  });
+};
+
+cdp.renderAll = function () {
+  cdp.renderUserMenuLinks();
+};
+
+window.cdp = cdp;
+})();
